de799480f2810492a3ce203cef25431b7b24b973
Andreas <dk.andreas@gmail.com>
2018-10-14 14:49:39 +0200

    Started work on OBD_UART lib

---
+++ ElectrifyEverything/src/obd_uart.c
@@ -0,0 +1,6 @@
+/*
+ * obd_uart.c
+ *
+ * Created: 14-10-2018 14:47:18
+ *  Author: Andreas
+ */ 
---
+++ ElectrifyEverything/src/obd_uart.h
@@ -0,0 +1,11 @@
+/*
+ * obd_uart.h
+ *
+ * Created: 14-10-2018 14:47:18
+ *  Author: Andreas
+ */ 
+
+#ifndef OBD_UART_H_
+#define OBD_UART_H_
+
+#endif /* OBD_UART_H_ */
